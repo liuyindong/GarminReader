d44040e9f3a73ce7769684601a90b682f2555b09
Olin Blodgett <nilo@nilosplace.net>
2008-06-13 19:46:11 +0000

    More Input Code

--- src/net/nilosplace/GarminReader/GarminReader.java
+++ src/net/nilosplace/GarminReader/GarminReader.java
@@ -1,11 +1,27 @@
 package net.nilosplace.GarminReader;
 
 
+import java.util.Enumeration;
+
+import javax.comm.CommPortIdentifier;
+
 public class GarminReader {
 	
 	public GarminReader() {
-		System.out.println("This is a test");
-		
+	    Enumeration pList = CommPortIdentifier.getPortIdentifiers();
+	    System.out.println(pList.hasMoreElements());
+	    while (pList.hasMoreElements()) {
+	      CommPortIdentifier cpi = (CommPortIdentifier) pList.nextElement();
+	      System.out.print("Port " + cpi.getName() + " ");
+	      if (cpi.getPortType() == CommPortIdentifier.PORT_SERIAL) {
+	        System.out.println("is a Serial Port: " + cpi);
+	      } else if (cpi.getPortType() == CommPortIdentifier.PORT_PARALLEL) {
+	        System.out.println("is a Parallel Port: " + cpi);
+	      } else {
+	        System.out.println("is an Unknown Port: " + cpi);
+	      }
+	    }
+	  
 	}
 
     public static void main(String[] args) {
