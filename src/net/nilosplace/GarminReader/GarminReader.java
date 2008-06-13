8258d4dd6746a15caf7bf91f4c2e35376f59489b
Olin Blodgett <nilo@nilosplace.net>
2008-06-13 19:16:12 +0000

    Inital Code for Garmin Reader

---
+++ src/net/nilosplace/GarminReader/GarminReader.java
@@ -0,0 +1,15 @@
+package net.nilosplace.GarminReader;
+
+
+public class GarminReader {
+	
+	public GarminReader() {
+		System.out.println("This is a test");
+		
+	}
+
+    public static void main(String[] args) {
+    	GarminReader reader = new GarminReader();
+    	
+     }
+}
