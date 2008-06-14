51126650abd7f705ea04de1005a2ab7cb12a0226
Olin Blodgett <nilo@nilosplace.net>
2008-06-14 00:08:27 +0000

    Code Additions plus a working reader

--- src/net/nilosplace/GarminReader/GarminReader.java
+++ src/net/nilosplace/GarminReader/GarminReader.java
@@ -1,31 +1,75 @@
 package net.nilosplace.GarminReader;
 
 
-import java.util.Enumeration;
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import javax.comm.CommPortIdentifier;
+import javax.comm.NoSuchPortException;
+import javax.comm.PortInUseException;
+import javax.comm.SerialPort;
 
-public class GarminReader {
+import dk.itu.haas.GPS.IDate;
+import dk.itu.haas.GPS.IGPSlistener;
+import dk.itu.haas.GPS.IPosition;
+import dk.itu.haas.GPS.ITime;
+import dk.itu.haas.GPS.Garmin.GarminGPS;
+
+public class GarminReader implements IGPSlistener {
+	
+	private GregorianCalendar cal;
+	private SimpleDateFormat format;
 	
 	public GarminReader() {
-	    Enumeration pList = CommPortIdentifier.getPortIdentifiers();
-	    System.out.println(pList.hasMoreElements());
-	    while (pList.hasMoreElements()) {
-	      CommPortIdentifier cpi = (CommPortIdentifier) pList.nextElement();
-	      System.out.print("Port " + cpi.getName() + " ");
-	      if (cpi.getPortType() == CommPortIdentifier.PORT_SERIAL) {
-	        System.out.println("is a Serial Port: " + cpi);
-	      } else if (cpi.getPortType() == CommPortIdentifier.PORT_PARALLEL) {
-	        System.out.println("is a Parallel Port: " + cpi);
-	      } else {
-	        System.out.println("is an Unknown Port: " + cpi);
-	      }
-	    }
-	  
+        CommPortIdentifier port;
+        format = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
+        cal = new GregorianCalendar(new Locale("en"));
+        cal.setTimeZone(TimeZone.getTimeZone("GMT"));
+        cal.set(Calendar.ZONE_OFFSET, 0);
+        cal.set(Calendar.DST_OFFSET, 0);
+
+		try {
+			port = CommPortIdentifier.getPortIdentifier("COM3");
+			SerialPort port2 = (SerialPort)port.open("ComControl", 3000);
+			GarminGPS gps = new GarminGPS(new BufferedInputStream(port2.getInputStream()), new BufferedOutputStream(port2.getOutputStream()));
+			gps.addGPSlistener(this);
+            gps.setAutoTransmit(true);
+            gps.requestDate();
+            gps.run();
+		} catch (NoSuchPortException e) {
+			e.printStackTrace();
+		} catch (PortInUseException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
     public static void main(String[] args) {
     	GarminReader reader = new GarminReader();
-    	
-     }
+    }
+
+	public void dateReceived(IDate arg0) {
+		cal.set(Calendar.YEAR, arg0.getYear());
+		cal.set(Calendar.MONTH, arg0.getMonth() - 1);
+		cal.set(Calendar.DAY_OF_MONTH, arg0.getDay());
+	}
+
+	public void positionReceived(IPosition arg0) {
+		System.out.println(arg0.getLatitude() + " " + arg0.getLongitude());
+	}
+
+	public void timeReceived(ITime arg0) {
+		cal.set(Calendar.HOUR_OF_DAY, arg0.getHours());
+		cal.set(Calendar.MINUTE, arg0.getMinutes());
+		cal.set(Calendar.SECOND, arg0.getSeconds());
+        System.out.println(this.format.format(cal.getTime()));
+	}
+
 }
