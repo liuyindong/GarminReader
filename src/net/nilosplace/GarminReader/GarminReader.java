dd95f559e50bcb26bc2bb9771f097f18e869cdb1
Olin Blodgett <nilo@nilosplace.net>
2008-06-26 00:55:58 +0000

    *** empty log message ***

--- src/net/nilosplace/GarminReader/GarminReader.java
+++ src/net/nilosplace/GarminReader/GarminReader.java
@@ -42,8 +42,9 @@ public class GarminReader implements IGPSlistener {
 		try {
 			Calendar filecal = Calendar.getInstance();
 			String filename = filecal.getTime().toString().replaceAll(" ", "_").replaceAll(":", "_") + ".txt";
-			FileWriter fw = new FileWriter("files/" + filename);
+			FileWriter fw = new FileWriter("files/" + filename + ".gpx");
 	        bw = new BufferedWriter(fw);
+	        DoFileHeader();
 			port = CommPortIdentifier.getPortIdentifier("COM6");
 			SerialPort port2 = (SerialPort)port.open("ComControl", 2000);
             port2.setSerialPortParams(9600,SerialPort.DATABITS_8,SerialPort.STOPBITS_1,SerialPort.PARITY_NONE);
@@ -61,7 +62,22 @@ public class GarminReader implements IGPSlistener {
 		}
 	}
 
-    public static void main(String[] args) {
+    private void DoFileHeader() {
+    	try {
+	    	bw.write("<gpx\r\n");
+	    	bw.write("\txmlns=\"http://www.topografix.com/GPX/1/0\"\r\n");
+	    	bw.write("\tversion=\"1.0\" creator=\"TopoFusion 3.15\"\r\n");
+	    	bw.write("\txmlns:TopoFusion=\"http://www.TopoFusion.com\"\r\n");
+	    	bw.write("\txmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"\r\n");
+	    	bw.write("\txsi:schemaLocation=\"http://www.topografix.com/GPX/1/0 http://www.topografix.com/GPX/1/0/gpx.xsd http://www.TopoFusion.com http://www.TopoFusion.com/topofusion.xsd\">\r\n");
+    	} catch (Exception e) {
+    		
+    	}
+
+		
+	}
+
+	public static void main(String[] args) {
     	GarminReader reader = new GarminReader();
     }
 
